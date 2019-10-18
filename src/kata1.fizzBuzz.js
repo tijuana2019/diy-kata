5c1583043e6deb8d031d14befa3319d3193fe0dc
Jo <jo_serendipity@icloud.com>
2019-10-18 18:39:12 +0100

    Tests and code written for Kata1

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,13 +1,14 @@
+/* eslint-disable no-else-return */
 const fizzBuzz = number => {
-    if (number % 3 === 0) {
-        return "Fizz";
-    } else if (number % 5 === 0) {
-        return "Buzz";
-    } else if (number % 3 === 0 && number % 5 === 0) {
-        return "FizzBuzz";
-    } else {
-        return number;
-    }
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "FizzBuzz";
+  } else if (number % 3 === 0) {
+    return "Fizz";
+  } else if (number % 5 === 0) {
+    return "Buzz";
+  } else {
+    return number;
+  }
 };
 
 module.exports = fizzBuzz;
--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -1,11 +1,32 @@
 const { fizzBuzz } = require("../src");
 
+const number = 0;
+
 describe("fizzBuzz", () => {
-  test("returns Fizz when passed a multiple of 3", () => {});
+  test("returns Fizz when passed a multiple of 3", () => {
+    expect(fizzBuzz(9)).toEqual("Fizz");
+    expect(fizzBuzz(3)).toEqual("Fizz");
+    expect(fizzBuzz(12)).toEqual("Fizz");
+  });
 
-  test("returns Buzz when passed a multiple of 5", () => {});
+  test("returns Buzz when passed a multiple of 5", () => {
+    expect(fizzBuzz(10)).toEqual("Buzz");
+    expect(fizzBuzz(100)).toEqual("Buzz");
+    expect(fizzBuzz(50)).toEqual("Buzz");
+  });
 
-  test("returns FizzBuzz when passed a multiple 3 and 5", () => {});
+  test("returns FizzBuzz when passed a multiple 3 and 5", () => {
+    expect(fizzBuzz(15)).toEqual("FizzBuzz");
+    expect(fizzBuzz(30)).toEqual("FizzBuzz");
+    expect(fizzBuzz(45)).toEqual("FizzBuzz");
+    expect(fizzBuzz(60)).toEqual("FizzBuzz");
+    expect(fizzBuzz(75)).toEqual("FizzBuzz");
+    expect(fizzBuzz(90)).toEqual("FizzBuzz");
+  });
 
-  test("returns the number when it isn't a multiple of 3 or 5", () => {});
+  test("returns the number when it isn't a multiple of 3 or 5", () => {
+    expect(fizzBuzz(7)).toEqual(7);
+    expect(fizzBuzz(8)).toEqual(8);
+    expect(fizzBuzz(67)).toEqual(67);
+  });
 });
